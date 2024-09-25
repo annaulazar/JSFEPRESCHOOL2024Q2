70ee12209db0ade6be0ff2c540f1d1ebb2b37a5f
Anna Lazarenko <aulazar@yandex.ru>
2024-09-26 00:55:36 +0700

    feat: add progress and rewind

--- audio-player/script.js
+++ audio-player/script.js
@@ -3,6 +3,10 @@ const playButton = document.querySelector(".btn-play");
 const playButtonImg = document.querySelector(".btn-play>img");
 const prevButton = document.querySelector(".btn-prev");
 const nextButton = document.querySelector(".btn-next");
+const progressBar = document.getElementById('progress-bar');
+const timeCurrent = document.querySelector(".time__current");
+const timeDuration = document.querySelector(".time__all");
+
 
 const songs = [
     {
@@ -18,7 +22,16 @@ const songs = [
         song: 'dontstartnow.mp3'
     }
 ]
+
+// console.log(audio.srcElement);
 let currentSong = 0;
+
+function getTime(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds - minutes * 60);
+    seconds = seconds >= 10 ? seconds : '0' + seconds;
+    return `${minutes}:${seconds}`;
+}
 
 function setSong(ind) {
     document.querySelector(".song__author").innerHTML = songs[ind].author;
@@ -26,6 +39,10 @@ function setSong(ind) {
     document.querySelector(".cover__img").src = `assets/img/${songs[ind].img}`;
     document.querySelector(".blur").style.backgroundImage = `url(assets/img/${songs[ind].img})`;
     audio.src = `assets/audio/${songs[ind].song}`;
+    audio.onloadedmetadata = () => {
+        let duration = audio.duration;
+        timeDuration.innerHTML = getTime(duration);
+    };
 }
 
 setSong(currentSong);
@@ -50,16 +67,39 @@ playButton.addEventListener('click', () => {
     }
 })
 
-prevButton.addEventListener('click', () => {
+function next() {
     currentSong++;
     if (currentSong > songs.length - 1) currentSong = 0;
     setSong(currentSong);
     play();
-})
+}
+nextButton.addEventListener('click', next)
 
-nextButton.addEventListener('click', () => {
+function prev() {
     currentSong--;
     if (currentSong < 0) currentSong = songs.length - 1;
     setSong(currentSong);
     play();
-})
+}
+
+prevButton.addEventListener('click', prev)
+
+function progress(e) {
+    const {duration, currentTime} = e.target;
+    let progress = currentTime / duration * 100;
+    progressBar.value = progress ? progress : 0;
+    let time = currentTime ? currentTime : 0;
+    timeCurrent.innerHTML = getTime(time);
+
+}
+
+audio.addEventListener('timeupdate', progress)
+
+function setTime(val) {
+    let duration = audio.duration;
+    let time = val / 100 * duration;
+    audio.currentTime = time;
+    timeCurrent.innerHTML = getTime(time);
+}
+
+audio.addEventListener('ended', next);
